553c5007295e02ca5066e9988b74525babd4ba59
Apverma0094 <abhishekvermajasrpur@gmail.com>
2026-07-10 20:10:15 +0530

    Replace Nodemailer with Resend

--- utils/emailService.js
+++ utils/emailService.js
@@ -3,19 +3,26 @@ const {
 } = require("./otpConfig");
 
 const { Resend } = require("resend");
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-
-await resend.emails.send({
-  from: "onboarding@resend.dev",
-  to: email,
-  subject: "Your OTP Code",
-  html: `
-    <h2>Expense Tracker Login</h2>
-    <p>Your OTP is:</p>
-    <h1>${otp}</h1>
-    <p>Valid for ${OTP_EXPIRY_SECONDS} seconds.</p>
-  `,
-});
+async function sendOTPEmail(email, otp) {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Your OTP Code",
+      html: `
+        <h2>Expense Tracker Login</h2>
+        <p>Your OTP is:</p>
+        <h1>${otp}</h1>
+        <p>Valid for ${OTP_EXPIRY_SECONDS} seconds.</p>
+      `,
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+}
 
 module.exports = sendOTPEmail;
